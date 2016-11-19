2b0300c7ff741323244b2c11b58008c80815618e
miyu <mkmx12@uw.edu>
2016-11-18 16:50:38 -0800

    add order state bar

---
+++ resources/js/OrderStateBar.js
@@ -0,0 +1,18 @@
+/**
+ * Created by miyukimura on 11/18/16.
+ */
+
+
+function move() {
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+        }
+    }
+}
