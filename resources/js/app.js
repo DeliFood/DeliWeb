d695966d84ed35b5bd83e6babe85aebab5390cc7
fulilan <lanfuli007@gmail.com>
2016-11-28 18:27:54 -0800

    fix restaurant css

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,16 @@
 /**
  * Created by lanfuli on 10/29/16.
  */
+
+/*jslint node: true, vars: true, bitwise: true */
+/*global angular */
+/* find out more about jslint: http://www.jslint.com/help.html */
+
+"use strict";
+angular.module('myApp', [])
+    .controller('myCtrl', ['$scope', function($scope) {
+        $scope.count = 0;
+        $scope.myFunc = function() {
+            $scope.count++;
+        };
+    }]);
